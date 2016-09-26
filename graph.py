b3846d9117b38e9df188a79a0fe527ee3d3ce8a7
sdatla2 <sdatla2@illinois.edu>
2016-09-26 00:27:53 -0500

    Updating DFS for some reason

--- graph.py
+++ graph.py
@@ -5,14 +5,14 @@ def generateGraph(matrix, width, height):
     for i, j in maze.keys():
         if j > 0 and not matrix[i][j-1] == '%':
             maze[(i,j)].append(("L",(i,j-1)))
-            maze[(i,j-1)].append(("R",(i,j)))
+            # maze[(i,j-1)].append(("R",(i,j)))
         if j < width - 1 and not matrix[i][j+1] == '%':
             maze[(i,j)].append(("R",(i,j + 1)))
-            maze[(i,j+1)].append(("L",(i,j)))
-        if i < height - 1 and not matrix[i+1][j] == '%':
-            maze[(i,j)].append(("D",(i + 1, j)))
-            maze[(i+1,j)].append(("U",(i,j)))
+            # maze[(i,j+1)].append(("L",(i,j)))
         if i > 0 and not matrix[i-1][j] == '%':
             maze[(i,j)].append(("U",(i - 1, j)))
-            maze[(i-1,j)].append(("D",(i,j)))
+            # maze[(i-1,j)].append(("D",(i,j)))
+        if i < height - 1 and not matrix[i+1][j] == '%':
+            maze[(i,j)].append(("D",(i + 1, j)))
+            # maze[(i+1,j)].append(("U",(i,j)))
     return maze
