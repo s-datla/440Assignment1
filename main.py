46e675bbd91056bab8dfbd76b2a4a73c25e4c7a1
sdatla2 <sdatla2@illinois.edu>
2016-09-25 23:05:26 -0500

    Part 1.1 Finished' '

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 import os.path
 import graph
 import collections
+import heapq
 
 searchoption = 0
 
@@ -52,30 +53,28 @@ def generateMaze(maze):
     return mazematrix
 
 def menu(option, mazematrix):
+    maze = graph.generateGraph(mazematrix,mazeWidth,mazeHeight)
     if option > 4 or option < 1:
         print "Incorrect parameter: \n1 <= Option <= 4"
         sys.exit(2)
-    elif option == 1:
-        BFS(mazematrix)
     elif option == 2:
-        DFS()
+        BFS(maze, mazematrix)
+    elif option == 1:
+        DFS(maze,mazematrix)
     elif option == 3:
-        greedy()
+        greedy(maze,mazematrix)
     elif option == 4:
-        aStar()
+        aStar(maze,mazematrix)
 
-
-def BFS(mazematrix):
-    maze = graph.generateGraph(mazematrix,mazeWidth,mazeHeight)
-    result = traverseBFS(maze)
+def displayFinal(mazematrix, optimal, numberExp):
     currentY, currentX = pacman
-    print "Optimal Path: \n" + result[0]
-    print "Optimal Path Length: " + str(len(result[0]))
-    print "Number of Expansions: " + str(result[1])
+    print "Optimal Path: \n" + optimal
+    print "Optimal Path Length: " + str(len(optimal))
+    print "Number of Expansions: " + str(numberExp)
 
-    for i in range(len(mazematrix)):
-        print "".join(mazematrix[i])
-    for j in list(result[0]):
+    # for i in range(len(mazematrix)):
+    #     print "".join(mazematrix[i])
+    for j in list(optimal):
 
         if j == 'L':
             currentX -= 1
@@ -89,6 +88,10 @@ def BFS(mazematrix):
     for i in range(len(mazematrix)):
         print "".join(mazematrix[i])
 
+def BFS(maze, mazematrix):
+    result = traverseBFS(maze)
+    displayFinal(mazematrix, result[0], result[1])
+
 
 def traverseBFS(maze):
     numberExp = 0
@@ -101,19 +104,79 @@ def traverseBFS(maze):
         if currentNode in marked:
             continue
         marked.add(currentNode)
-        for direction, neighbour in maze[currentNode]:
-            q.append((currentPath + direction, neighbour))
-            numberExp += 1
+        numberExp += 1
+        for move, nextNode in maze[currentNode]:
+            q.append((currentPath + move, nextNode))
     return "Unexpected Function Return"
 
-def DFS():
-    return 1
+def DFS(maze, mazematrix):
+    result = traverseDFS(maze)
+    displayFinal(mazematrix, result[0], result[1])
 
-def greedy():
-    return 1
 
-def aStar():
-    return 1
+def traverseDFS(maze):
+    numberExp = 0
+    s = collections.deque([("", pacman)])
+    marked = set()
+    while s:
+        currentPath, currentNode = s.pop()
+        if currentNode == goal:
+            return (currentPath, numberExp)
+        if currentNode in marked:
+            continue
+        marked.add(currentNode)
+        numberExp += 1
+        for move, nextNode in maze[currentNode]:
+            s.append((currentPath + move, nextNode))
+
+    return "Unexpected Function Return"
+
+def manhattanDistance(pacman, goal):
+    return abs(goal[0] - pacman[0]) + abs(goal[1] - pacman[1])
+
+def greedy(maze,mazematrix):
+    result = traverseGreedy(maze)
+    displayFinal(mazematrix, result[0], result[1])
+
+def traverseGreedy(maze):
+    numberExp = 0
+    priorityQueue = []
+    heapq.heappush(priorityQueue, (manhattanDistance(pacman, goal), "", pacman))
+    marked = set()
+    while priorityQueue:
+        cost, currentPath, currentNode = heapq.heappop(priorityQueue)
+        if currentNode == goal:
+            return (currentPath, numberExp)
+        if currentNode in marked:
+            continue
+        marked.add(currentNode)
+        numberExp += 1
+        for move, nextNode in maze[currentNode]:
+            heapq.heappush(priorityQueue, (manhattanDistance(currentNode,goal),currentPath + move, nextNode))
+    return "Unexpected Function Return"
+
+
+
+def aStar(maze, mazematrix):
+    result = traverseAStar(maze)
+    displayFinal(mazematrix, result[0], result[1])
+
+def traverseAStar(maze):
+    numberExp = 0
+    priorityQueue = []
+    heapq.heappush(priorityQueue, (manhattanDistance(pacman,goal),0,"", pacman))
+    marked = set()
+    while priorityQueue:
+        _, cost, currentPath, currentNode = heapq.heappop(priorityQueue)
+        if currentNode == goal:
+            return (currentPath, numberExp)
+        if currentNode in marked:
+            continue
+        marked.add(currentNode)
+        numberExp += 1
+        for move, nextNode in maze[currentNode]:
+            heapq.heappush(priorityQueue, (cost + manhattanDistance(currentNode,goal), cost + 1, currentPath + move, nextNode))
+    return "Unexpected Function Return"
 
 
 if __name__ == '__main__':
